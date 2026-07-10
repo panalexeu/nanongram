d98882e63a5cb77fdefafbf66f4353ee8c14c5e6
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2026-07-10 18:18:15 +0300

    add regexp tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,26 @@
+import re 
+
+from preproc import START_SEQ, END_SEQ
+
+class BaseTokenizer:
+    """
+    regexp tokenizer with modified gpt2 expr to work with re
+    """
+    def __init__(self):
+        self.exp = rf"{START_SEQ}| ?{END_SEQ}|'s|'t|'re|'ve|'m|'ll|'d| ?\w+| ?[^\s\w]+|\s+(?!\S)|\s+" 
+                     # special tokens        # clitics               # a-n #  non a-n # punct   # whitespace
+        self.pat = re.compile(self.exp)
+
+    def __call__(self, text: str, lower: bool = True) -> list[str]: 
+        text = text.strip() 
+        if lower: 
+            text = text.lower() 
+
+        return re.findall(self.pat, text)
+
+if __name__ == '__main__': 
+    tokenizer = BaseTokenizer()
+    with open('./out.txt', 'r') as f:
+        for line in f.readlines():
+            res = tokenizer(line)
+            breakpoint()
